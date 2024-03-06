dd91d115c696d8aa94c3a338f989c17c3157d4b6
arun2352000 <poorvaja10@gmail.com>
2024-03-07 01:05:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import fs from "fs";
+import { format } from "date-fns";
+const app = express();
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`app is running in the port: ${PORT}`);
+}); 
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "hai Arunachalam" });
+});
+app.get("/write", (req, res) => {
+  let today = format(new Date(), "dd-mm-yyyy-HH-mm");
+  console.log(("today", today));
+  const filePath = `Timestamp/${today}.txt`;
+  fs.writeFileSync(filePath, `${today}`, "utf8");
+  res.status(200).json({ message: "hai data writen" });
+  //   let data = fs.readFileSync(filePath, "utf8");
+  //     res.status(200), send(data);
+});
+
+app.get("/read", (req, res) => {
+  let today = format(new Date(), "dd-mm-yyyy-HH-mm");
+  const filePath = `Timestamp/${today}.txt`;
+  let data = fs.readFileSync(filePath, "utf8");
+  res.status(200).send(data);
+});
